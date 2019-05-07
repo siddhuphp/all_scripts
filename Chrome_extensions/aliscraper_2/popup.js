d7c888cddb6104e9f8e68f4962328ea0e4c07bfa
siddhuphp <siddhu.php@gmail.com>
2019-05-07 18:55:40 +0530

    Progress :)

--- Chrome_extensions/aliscraper_2/popup.js
+++ Chrome_extensions/aliscraper_2/popup.js
@@ -11,6 +11,7 @@ console.log("POPUP js fired");
       if (request.greeting == "hello")
       {
         sendResponse({farewell: "goodbye"});
+        $("#product_id").text(request.final.product_id);
         $("#product_title").text(request.final.product_title);
         $("#product_price").text(request.final.product_price);
         $("#product_discount_price").text(request.final.product_discount_price); 
@@ -19,25 +20,48 @@ console.log("POPUP js fired");
             $("#product_images").append('<img src="'+value+'">&nbsp;&nbsp;');
           });
           
-          
+          $("#product_images").append('<hr>');
           $.each(request.final.product_item_list, function( index, value ) {
-            $("#products_item_options").append('<h4>'+value.item_name+'</h4>');
+            $("#products_item_options").append('<h4>'+value.item_name+'</h4>');           
             if(value.item_values)
             {
               $.each(value.item_values, function( k, v ) {
-                if(value.item_type == "TEXT-TYPE")
+                if(value.item_type == "TEXT")
                 {
                   $("#products_item_options").append('<p>'+v+'</p>');
                 }
-                else if(value.item_type == "IMG-TYPE")
+                else if(value.item_type == "IMAGE")
                 {
                   $("#products_item_options").append('<img src="'+v+'">&nbsp;&nbsp;');
                 }                
               });
             }
+            $("#products_item_options").append('<p><b>Item Type: </b>'+value.item_type+'</p>');
           });
+          $("#products_item_options").append('<hr>');
 
-              
+          // Product details
+          $("#products_item_options").append('<h4>'+request.final.product_detail_title+'</h4>');
+          $("#products_item_options").append('<p><b>'+request.final.product_detail_sub_title+'</b></p>');
+          $.each(request.final.product_detail_list, function( index, value ) {
+            $("#products_item_options").append('<p>'+value+'</p>');
+          });
+          
+          //India Shipping Details
+          if(request.final.product_id)
+          {
+            $.ajax({
+              url: "https://freight.aliexpress.com/ajaxFreightCalculateService.htm?productid="+request.final.product_id+"&country=IN",
+              type: 'GET',
+              crossDomain:true,
+              cache:false,
+              async:false,
+              //dataType: 'json', // added data type
+              success: function(res) {
+                  console.log(res);                 
+              }
+            });
+          }
       }
         
     });
--- Chrome_extensions/aliscraper_2/scripts/content.js
+++ Chrome_extensions/aliscraper_2/scripts/content.js
@@ -8,11 +8,11 @@ function gotMessage(message){
 function products(){
 
 let product_title = validate_xpath('//*[@id="j-product-detail-bd"]/div[1]/div/h1');
-
 let product_price = validate_xpath('//*[@id="j-sku-price"]');
-
 let product_discount_price = validate_xpath('//*[@id="j-sku-discount-price"]');
-
+let product_detail_title = validate_xpath('//div[@class="main-content"]/div[@id="j-product-tabbed-pane"]/ul/li[1]/a');
+let product_detail_sub_title = validate_xpath('//div[@class="main-content"]/div[@id="j-product-tabbed-pane"]/div/div/div/div[2]/div[@class="ui-box-title"]');
+let product_id = ($("#hid-product-id").val())?($("#hid-product-id").val()):0;
 //console.log("product_title"+product_title + "product_price"+product_price + "product_discount_price"+product_discount_price);
 
 let product_images = new Array();
@@ -23,11 +23,15 @@ let product_images = new Array();
 
 
   var data = {
+      "product_id":product_id,
       "product_title":product_title,
       "product_price":product_price,
       "product_discount_price":product_discount_price,
       "product_images":product_images,
       "product_item_list":list_items(),
+      "product_detail_title":product_detail_title,
+      "product_detail_sub_title":product_detail_sub_title,
+      "product_detail_list":get_product_details(),
   };
 
   chrome.runtime.sendMessage({greeting: "hello",final:data}, function(response) {
@@ -56,7 +60,7 @@ function validate_xpath_only(xpath)
   return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null).iterateNext() instanceof Node;  
 }
 
-
+// This is for item list details
 function list_items()
 {
 	if(validate_xpath_only('//*[@id="j-product-info-sku"]')) // check exist or not
@@ -105,11 +109,11 @@ function check_type(key)
 	type = ' - ';
 	if(validate_xpath_only('//*[@id="j-product-info-sku"]/dl['+key+']/dd/ul/li/a/img/@src'))
 	{
-		type = "IMG-TYPE";
+		type = "IMAGE";
 	}
 	else if(validate_xpath_only('//*[@id="j-product-info-sku"]/dl['+key+']/dd/ul/li/a/span'))
 	{
-		type = "TEXT-TYPE";
+		type = "TEXT";
 	}
 	return type;
 }
@@ -117,11 +121,11 @@ function check_type(key)
 
 function get_item_values(key)
 {
-	if(check_type(key) == "IMG-TYPE")
+	if(check_type(key) == "IMAGE")
 	{
 		return get_images_data(key);
 	}
-	else if(check_type(key) == "TEXT-TYPE")
+	else if(check_type(key) == "TEXT")
 	{
 		return get_text_data(key);
 	}
@@ -153,6 +157,28 @@ function get_images_data(key)
 function get_text_data(key)
 {
 	var iterator = document.evaluate('//*[@id="j-product-info-sku"]/dl['+key+']/dd/ul/li/a/span', document, null, XPathResult.ANY_TYPE, null );
+	try {
+		  var thisNode = iterator.iterateNext();
+		  var list_items = [];		  
+		  var i = 0;
+		  while (thisNode) {
+					if(thisNode.textContent !== null && thisNode.textContent !== '') {
+					 list_items[i] = thisNode.textContent;
+					 thisNode = iterator.iterateNext();
+						i++;
+					}
+			  } 
+		}
+		catch (e) {
+		  console.log( 'Error: Document tree modified during iteration ' + e );
+		}
+		return list_items;
+}
+
+
+function get_product_details()
+{
+	var iterator = document.evaluate('//div[@class="main-content"]/div[@id="j-product-tabbed-pane"]/div/div/div/div[2]/div[2]/ul/li', document, null, XPathResult.ANY_TYPE, null );
 	try {
 		  var thisNode = iterator.iterateNext();
 		  var list_items = [];		  
