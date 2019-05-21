254411946a2d9e5ba9a455d8a61047c4e7cc1275
siddhuphp <siddhu.php@gmail.com>
2019-05-21 18:19:49 +0530

    progress

--- Chrome_extensions/aliscraper_4/background.js
+++ Chrome_extensions/aliscraper_4/background.js
@@ -174,16 +174,27 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
  function tst(){
    var xhr = new XMLHttpRequest();
    xhr.open("GET", "https://www.aliexpress.com/item/BIANYILONG-2019-t-shirt-men-Newest-Venom-Marvel-t-shirt-3D-Printed-T-shirts-Men-Women/32972594727.html?spm=2114.search0103.3.9.13351a9788kZlF&ws_ab_test=searchweb0_0,searchweb201602_5_10065_10068_319_10059_10884_317_10887_10696_321_322_10084_453_10083_454_10103_10618_10307_537_536,searchweb201603_52,ppcSwitch_0&algo_expid=a44de6fc-ddc4-4ac1-834d-bbd0ad84acab-1&algo_pvid=a44de6fc-ddc4-4ac1-834d-bbd0ad84acab&transAbTest=ae803_3", true);
+   xhr.responseType="document";
    xhr.onreadystatechange = function() {
-     if (xhr.readyState == 4) {
-       // WARNING! Might be injecting a malicious script!
-      // document.getElementById("resp").innerHTML = xhr.responseText;
-       console.log(xhr.responseText);
-       
-     }
+     if (xhr.readyState == 4) {      
+       //console.log(xhr.response);     
+       value = document.evaluate('//*[@id="j-product-detail-bd"]/div[1]/div/h1', xhr.response, null, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+       //console.log(value);
+       //let sku_details = validate_xpath("//div[@class='detail-wrap'][1]/script[1]");
+       sku_details = document.evaluate("//div[@class='detail-wrap'][1]/script[1]", xhr.response, null, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+       //console.log(sku_details);
+       var b = /(?<=skuProducts=)(.*)(?=;)/g.exec(sku_details);
+         var SKUVariants=JSON.parse(b[0]);
+         console.log(b[0]);
+      }
    }
    xhr.send();
-
  }
 
- //tst();
+ tst();
+
+
+ function make_data()
+ {
+   
+ }
