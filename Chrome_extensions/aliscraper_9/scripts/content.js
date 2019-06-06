d75792b3e78cbecf50c34aca47ad6dbecd53b3fe
siddhuphp <siddhu.php@gmail.com>
2019-06-06 18:50:25 +0530

    progress

--- Chrome_extensions/aliscraper_9/scripts/content.js
+++ Chrome_extensions/aliscraper_9/scripts/content.js
@@ -1,5 +1,4 @@
 chrome.runtime.onMessage.addListener(gotMessage);
-
 function gotMessage(message){
     console.log(message.msg);
     //below request send data to popup js. 
@@ -8,53 +7,33 @@ function gotMessage(message){
 		}); 
 }
 
-//  var cssLink = document.createElement('link');  
-//  cssLink.setAttribute('rel','stylesheet');
-//  cssLink.setAttribute('href','https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css');
-//  document.head.appendChild(cssLink);
-
-//  var jQueryScript = document.createElement('script');  
-//  jQueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js');
-//  document.head.appendChild(jQueryScript);
-
-//  var jQueryScript = document.createElement('script');  
-//  jQueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.5/umd/popper.js');
-//  document.head.appendChild(jQueryScript);
-
-//  var jQueryScript = document.createElement('script');  
-//  jQueryScript.setAttribute('src','https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.js');
-//  document.head.appendChild(jQueryScript);
-
-
-
-//  var cssLink2 = document.createElement('link');  
-//  cssLink2.setAttribute('rel','stylesheet');
-//  cssLink2.setAttribute('href','https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css');
-//  document.head.appendChild(cssLink2);
-
-//  var jQueryScript1 = document.createElement('script');  
-//  jQueryScript1.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.js');
-//  document.head.appendChild(jQueryScript1);
-
-
-
-
-
-var dd = getElementByXpath("//div[@id='j-product-description']/div[@class='ui-box-title']");
-dd.insertAdjacentHTML('beforeend', '<button id="editor_call"> Edit </button> &nbsp; &nbsp; &nbsp; <button id="preview_call"> Preview </button>');
+var dd = getElementByXpath('//div[@class="main-content"]/div/div/div/div[@id="j-product-desc"]');
+dd.insertAdjacentHTML('beforebegin', '<button id="editor_call"> Edit </button> &nbsp; &nbsp; &nbsp; <button id="preview_call"> Preview </button><br/>');
 
 
 function getElementByXpath(path) {
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+	if(validate_xpath_only(path))
+	{
+		return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+	}
+	else
+	{
+		console.log("Failed to read xpath in content.js 21 line");
+		return false;
+	}
+  
+}
+
+function validate_xpath_only(xpath)
+{
+  return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null).iterateNext() instanceof Node;  
 }
 
 
-
-
 document.getElementById ("editor_call").addEventListener("click", editaction, false);
-
 function editaction() {
-	$('.description-content').summernote({
+	remove_div_products();
+	$('#j-product-desc').summernote({
 		height: 300,
 		tabsize: 2,
 		followingToolbar: true,
@@ -62,15 +41,65 @@ function editaction() {
 }
 
 document.getElementById ("preview_call").addEventListener("click", previewaction, false);
-
 function previewaction() {
-	$('.description-content').summernote('destroy');
+	$('#j-product-desc').summernote('destroy');
 }
 
 
 
+function when_page_loads()
+{
+	// product-name
+	$('#target').replaceWith('<textarea name="product_title">' + $('target').html() +'</textarea>')
+}
 
 
+function remove_div_products()
+{
+
+	try
+	{
+	var allListElements = $( 'div' );
+	var t = $( ".origin-part" ).find( allListElements );
+	
+
+	if ($('.detailmodule_dynamic').length) {
+		$('.detailmodule_dynamic').remove();// to remove unwated data
+	}
+	else if(t.length > 0)
+	{
+		
+		//t.first().css( "background-color", "red" );
+		x=t.first().attr('style');
+		
+		if (x)
+		{
+		if(x.indexOf("overflow:")>0){
+			t.first().remove(); 
+		}  
+	}  
+	}	
+	else 
+	{
+		if ($('.origin-part div').length)
+		{
+			var styletext=$('.origin-part div').attr('style');
+			if (styletext.length>0)
+			{
+			if (styletext.indexOf("overflow")==0)
+			{
+				$('.origin-part div').remove();
+			}
+		}
+		}
+	}
+}
+catch (e)
+{
+	console.log( e);
+	return;
+}
+}
 
 
 
